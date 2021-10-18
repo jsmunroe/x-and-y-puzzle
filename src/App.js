72a5522887557ece28e852cc0721f7d01e9804df
Jordan Munroe <Jordan.Munroe@ioausa.com>
2021-10-18 15:55:43 -0400

    Added a click counter.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ const initCards = count => Array(count).fill({}).map((e,i) => ({ index: i, faceD
 
 function App() {
     const [cards, setCards] = useState(initCards(10))
+    const [clicks, setClicks] = useState(0);
 
     const handleCardClick = index => {
         if (cards[index].faceDown === true) {
+            setClicks(clicks => clicks + 1);
             setCards(
                 cards.map(c => c.index === index ? {...c, faceDown: false} : c ) // Turn the clicked card face-up.
                      .map(c => c.index === index + 1 ? {...c, faceDown: !c.faceDown} : c) // Toggle the card to the right if it exists.
@@ -16,7 +18,10 @@ function App() {
         }
     }
 
-    return <Tableau cards={cards} onCardClick={handleCardClick}/>;
+    return <div className="app">
+        <Tableau cards={cards} onCardClick={handleCardClick}/>
+        <span className="clicks">{clicks}</span>
+    </div>;
 }
 
 export default App;
